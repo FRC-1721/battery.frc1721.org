8822e9bf06a005f07abbd71d60aae76198b757ea
dublUayaychtee <mail@dublu.net>
2025-01-11 00:53:01 -0500

    display first_name instead of username, additional formatting

--- log/views.py
+++ log/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Entry
 from .forms import EntryForm
+
 from battery.settings import BAD_KEY
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -18,19 +21,28 @@ def index(request):
         if form.is_valid():
             data = form.cleaned_data
             entry = Entry(
-                battery = data['battery'],
-                ready = data['ready'],
+                battery   = data['battery'],
+                ready     = data['ready'],
                 condition = data['condition'],
-                charge = data['charge'],
-                rint = data['rint'],
-                memo = data['memo'],
-                user=request.user.username
+                charge    = data['charge'],
+                rint      = data['rint'],
+                memo      = data['memo'],
+                user      = request.user.username
             )
             entry.save()
 
-        
         return HttpResponseRedirect("/")
 
-    entries = Entry.objects.all()
+
+    entries = list(map(
+        lambda e:
+            e.update({'user': (User.objects.filter(username=e['user']).first() or User(first_name=e['user'])).first_name})
+            or e.update({'condition': Entry.Condition(e['condition']).label or "N/A"})
+            or e.update({'memo': e['memo'] or ''})
+            or e.update({'date': e['date'].strftime("%d %b, %Y %H:%M:%S")})
+            or e,
+        map(lambda x: x.__dict__, Entry.objects.order_by('date').reverse())
+    ))
+
     form = EntryForm()
     return render(request, 'index.html', {'entries': entries, 'form': form, 'bad_key': BAD_KEY})
